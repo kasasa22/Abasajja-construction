415294e372a380a13ec623c91903f8355c2bdd12
kasasa22 <kasasatrevor25@gmail.com>
2026-03-09 17:24:58 +0300

    refactor: update user model, admin panel provider, routes, and styles

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,42 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/plans', function () {
+    return view('plans');
+});
+
+Route::get('/plans/{id}', function ($id) {
+    return view('plan-detail', ['id' => $id]);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blog/{id}', function ($id) {
+    return view('blog-detail', ['id' => $id]);
+});
+
